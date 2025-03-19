ba1cf0d8d60377857cb83921a3ca918ef0f8a2e1
Jessica Adzoyi <121783593+JESSICA047@users.noreply.github.com>
2025-03-19 11:10:32 +0000

    Update Home page styles and layout
    - Modified Home.css to improve the overall styling of the Home page.
    - Updated Home.jsx to enhance the layout and structure.

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Navbar from "../../components/Navbar/Navbar";
 import "./Home.css";
 import "animate.css";
 
 function Home() {
+  const [roleIndex, setRoleIndex] = useState(0);
+  const [displayText, setDisplayText] = useState("");
+  const [isDeleting, setIsDeleting] = useState(false);
+  const roles = ["HEALTH-TECH ENTHUSIAST", "FRONT-END DEVELOPER"];
+  const typingSpeed = 100;
+  const deletingSpeed = 50;
+  const delayBetweenRoles = 1500;
+
+  useEffect(() => {
+    const timeout = setTimeout(
+      () => {
+        const currentRole = roles[roleIndex];
+
+        if (!isDeleting) {
+          setDisplayText(currentRole.substring(0, displayText.length + 1));
+
+          // Once we've typed the full role
+          if (displayText.length === currentRole.length) {
+            setTimeout(() => setIsDeleting(true), delayBetweenRoles);
+          }
+        } else {
+          setDisplayText(currentRole.substring(0, displayText.length - 1));
+
+          // Once we've deleted the role
+          if (displayText.length === 0) {
+            setIsDeleting(false);
+            setRoleIndex((prevIndex) => (prevIndex + 1) % roles.length);
+          }
+        }
+      },
+      isDeleting ? deletingSpeed : typingSpeed
+    );
+
+    return () => clearTimeout(timeout);
+  }, [displayText, isDeleting, roleIndex, roles]);
+
   return (
     <div className="home">
       <Navbar />
@@ -19,12 +55,18 @@ function Home() {
             <div className="intro-text animate__animated animate__fadeInRight">
               <p className="im-text">
                 I'M{" "}
-                <Link to="/resume" className="name">
+                <Link to="/about" className="name">
                   JESSICA
                 </Link>
-                ,
+                ,<p>AND I'M A </p>
               </p>
-              <p className="role-text">AND I'M A FRONT-END DEVELOPER.</p>
+              <p className="role-text">
+                <p>
+                  {" "}
+                  <span className="role-animation">{displayText}</span>
+                  <span className="cursor"></span>
+                </p>
+              </p>
             </div>
           </div>
         </section>
